65ac7182e6e640c53030ae9ab97045310b81a667
Aditi Khose <59204148+Aditikhose20@users.noreply.github.com>
2021-03-17 15:54:15 +0530

    Add files via upload

---
+++ db.sql
@@ -0,0 +1,7 @@
+create table student ( name varchar(30) not null, Rollno int primary key, Address varchar(50), Mobile_No numeric(10) not null, PAN_No varchar(8), primary key (Rollno) );
+
+insert into student values('Aditi', '19111021', 'pune', '9876546778', 'GBHG3460');
+insert into student values('shraddha', '19111212', 'mumbai', '89907890', 'YUHI5770');
+insert into student values('vinayak', '1911145', 'pune', '87696790', 'JKIL9770');
+insert into student values('kartik', '1911245', 'aurangabad', '7896456790', 'PDTRG9770');
+insert into student values('Atharva', '3245671', 'pune', '9798456790', 'DRTH6770');
