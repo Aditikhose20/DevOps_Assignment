df6bc4d6c564bdc40021b5a69adf29cf5cae584a
Aditi Khose <59204148+Aditikhose20@users.noreply.github.com>
2021-03-17 15:55:03 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,4 +1,4 @@
-create table student ( name varchar(30) not null, Rollno int primary key, Address varchar(50), Mobile_No numeric(10) not null, PAN_No varchar(8), primary key (Rollno) );
+create table student(name varchar(30) not null, Rollno int primary key, Address varchar(50), Mobile_No numeric(10) not null, PAN_No varchar(8));
 
 insert into student values('Aditi', '19111021', 'pune', '9876546778', 'GBHG3460');
 insert into student values('shraddha', '19111212', 'mumbai', '89907890', 'YUHI5770');
